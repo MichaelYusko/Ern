137717870348af855f85dd2370f057d05a705a13
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-05-05 14:11:14 +0300

    move json.py file to helpers directory, and add few Erorr classes

--- ern/errors.py
+++ ern/errors.py
@@ -7,4 +7,20 @@ class SlackApiError(Exception):
 
 
 class SlackChannelError(Exception):
-    pass
+    def __init__(self, channel):
+        self.channel = channel
+
+    def __str__(self):
+        return 'Channel {} nof found'.format(self.channel)
+
+
+class SlackUserError(Exception):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'User {} not found.'.format(self.name)
+
+
+class SlackTokenError(Exception):
+    def __str__(self): return 'Please provide your token'
